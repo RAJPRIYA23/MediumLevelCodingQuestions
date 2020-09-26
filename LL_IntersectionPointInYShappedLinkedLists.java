97cf70f5a7f023a975b43499c654f3992d52977c
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-26 21:53:34 +0530

    Create LL_IntersectionPointInYShappedLinkedLists.java

---
+++ LL_IntersectionPointInYShappedLinkedLists.java
@@ -0,0 +1,165 @@
+/*
+Given two singly linked lists of size N and M, write a program to get the point where two linked lists intersect each other.
+
+Example:
+
+Input:
+LinkList1 = {10,20,5,10}
+LinkList2 = {30,40,50,5,10}
+Output: 5
+Explanation:The point of intersection of
+two linked list is 5, means both of them
+get linked (intersects) with each other
+at node whose value is 5.
+Your Task:
+The task is to complete the function intersetPoint() which finds the point of intersection of two linked list. The function should return data value of a node where two linked lists merge. 
+If linked list do not merge at any point, then it should return -1.
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+	
+ class LinkedList_Intersection
+{
+    Node head = null;  
+	Node tail = null;
+
+public void addToTheLast(Node node) 
+{
+
+  if (head == null) {
+   head = node;
+   tail = head;
+  } else {
+   //Node temp = head;
+   //while (temp.next != null)
+    //temp = temp.next;
+
+   //temp.next = node;
+   tail.next=node;
+   tail = node;
+  }
+}
+
+  /* Function to print linked list */
+    void printList()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+       
+         
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			int n3 = sc.nextInt();
+			LinkedList_Intersection llist1 = new LinkedList_Intersection();
+		    LinkedList_Intersection llist2 = new LinkedList_Intersection();
+			LinkedList_Intersection llist3 = new LinkedList_Intersection();
+			
+				int a1=sc.nextInt();
+				Node head1= new Node(a1);
+				Node tail1= head1;
+				
+				for (int i = 1; i < n1; i++) 
+				{
+					int a = sc.nextInt(); 
+					tail1.next = (new Node(a));
+					tail1= tail1.next;
+				}
+			
+			
+				int b1=sc.nextInt();
+				Node head2 = new Node(b1);
+				Node tail2 = head2;
+				for (int i = 1; i < n2; i++) 
+				{
+					int b = sc.nextInt();  
+					tail2.next = (new Node(b));
+					tail2= tail2.next;
+				}
+				
+				int c1=sc.nextInt();
+				Node head3= new Node(c1);
+				tail1.next = head3;
+				tail2.next = head3;
+				Node tail3=head3;
+				for (int i = 1; i < n3; i++) 
+				{
+					int c = sc.nextInt();   
+					tail3.next = (new Node(c));
+					tail3= tail3.next;
+				}
+				
+				Intersect obj = new Intersect();
+				System.out.println(obj.intersectPoint(head1, head2));
+			t--;			
+         }
+    }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}
+This is method only submission.  You only need to complete the method. */
+class Intersect
+{
+	int intersectPoint(Node headA, Node headB)
+	{
+         Node tempA = headA, tempB = headB;
+         
+         while(tempA != null)
+         {
+             tempA.data = (tempA.data * -1) - 1;
+             tempA = tempA.next;
+         }
+         
+         while(tempB!= null)
+         {
+             if(tempB.data < 0 )
+                return -(tempB.data)-1;
+            tempB = tempB.next;
+         }
+         
+         return -1;
+	}
+}
