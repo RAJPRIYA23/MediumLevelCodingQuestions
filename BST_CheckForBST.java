59cc141f69216e0d62e439f2ccd9ce061d12eb59
Rajpriya <53468432+RAJPRIYA23@users.noreply.github.com>
2020-09-27 22:30:14 +0530

    Create BST_CheckForBST.java

---
+++ BST_CheckForBST.java
@@ -0,0 +1,193 @@
+/*
+Given a binary tree. Check whether it is a BST or not.
+
+Example 1:
+
+Input:
+    2
+ /    \
+1      3
+Output: 1
+Example 2:
+
+Input:
+  2
+   \
+    7
+     \
+      6
+       \
+        5
+         \
+          9
+           \
+            2
+             \
+              6
+Output: 0
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function isBST() which takes the root of the tree as a parameter and returns true if the given binary tree is BST, else returns false. 
+The printing is done by the driver's code.
+*/
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Initial Template for Java
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+        	    if(g.isBST(root))
+			        System.out.println(1);
+			    else
+			        System.out.println(0);
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+Structure of Node:
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+public class Tree
+{
+    ArrayList<Integer> inorder = new ArrayList<Integer>();
+        
+    void inOrder(Node root)
+    {
+        if(root != null)
+        {
+            inOrder(root.left);
+            inorder.add(root.data);
+            inOrder(root.right);
+        }
+    }
+    
+    boolean isBST(Node root)
+    {
+        inorder.clear();
+        inOrder(root);
+        
+        Boolean is_true = true;
+        for(int i = 0; i < inorder.size()-1; i++)
+        {
+            if(inorder.get(i) >= inorder.get(i+1))
+            {
+                is_true = false;
+                break;
+            }
+        }
+        
+        return is_true;
+    }
+}
